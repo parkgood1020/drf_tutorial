2dfe4a58786c43fb410d65d900e40c88216cea09
parkgood1020 <parkgood1020@gmail.com>
2022-02-05 15:47:55 +0900

    snippets serializers.py modify

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,15 +6,3 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
-
-    def create(self, validated_date):
-        return Snippet.objects.create(**validated_date)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
