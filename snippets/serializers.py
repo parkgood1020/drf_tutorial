3af280678501c0da20ed86fcc1538d2545888b52
parkgood1020 <parkgood1020@gmail.com>
2022-02-05 15:11:04 +0900

    add requirements.txt

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,17 +1,11 @@
 from rest_framework import serializers
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from .models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
-    # id = serializers.IntegerField(read_only=True)
-    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    # code = serializers.CharField(style={'base_template': 'textarea.html'})
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
     def create(self, validated_date):
         return Snippet.objects.create(**validated_date)
